37cbff57ec8707c1694d8ec09d279e79bfc4ce6a
Emanuele Ruffaldi <emanuele.ruffaldi@gmail.com>
2017-12-30 18:55:37 +0100

    dump and ls

--- resticcli.py
+++ resticcli.py
@@ -2,6 +2,7 @@
 from restic_decryptor import *
 import argparse
 import os
+import sys
 import json
 
 class PackRef:
@@ -11,6 +12,8 @@ class PackRef:
 class Index:
     def __init__(self,basepath,masterkey):
         path = os.path.join(basepath,"index")
+        self.basepath = basepath
+        self.masterkey =masterkey
         bb={}
         pp={}
         for x in os.listdir(path):
@@ -35,8 +38,52 @@ class Index:
                         bb[blob["id"]] = blob
         self.blobs = bb
         self.packs = pp
+    def get(self,id):
+        blob = self.blobs.get(id)
+        if blob is None:
+            return None
+        pr = blob["pack"]
+        if pr.fp is None:
+            pr.fp = open(os.path.join(self.basepath,"data",pr.id[0:2],pr.id),"rb")
+        pr.fp.seek(blob["offset"])
+        r = decrypt(self.masterkey, pr.fp.read(blob['length']))
+        return (blob,r)
 
-
+def find_path(parts,root,pi):
+    blob,t = pi.get(root)
+    if t is None:
+        return (None,None)
+    if blob["type"] != "tree":
+        return (None,None)
+    t = json.loads(t)
+    for n in t["nodes"]:
+        if n["name"] == parts[0]:
+            if len(parts) == 1:
+                return (n,root) # item and parent
+            elif n["type"] == "dir":
+                return find_path(parts[1:],n["subtree"],pi)
+            else:
+                break
+    return (None,None)
+def ls_recursive(root,prefix,pi):
+    blob,t = pi.get(root)
+    if t is None:
+        return
+    if blob["type"] != "tree":
+        return
+    t = json.loads(t)
+    for n in t["nodes"]:
+        # name
+        # type=dir/file
+        # content=null if file
+        # subtree=reference if dir
+        # size if fil
+        # extended_attributes
+        p = os.path.join(prefix,n["name"])
+        if n["type"] == "file":
+            print (p)
+        elif n["type"] == "dir":
+            ls_recursive(n["subtree"],p,pi)
 def get_snapshots(repo,masterkey):
     r = []
     bp = os.path.join(repo,"snapshots")
@@ -44,13 +91,29 @@ def get_snapshots(repo,masterkey):
         fp = os.path.join(bp,x)
         if os.path.isfile(fp):
             xx = decrypt_config_index_snapshot(masterkey,fp)
-            xx["id"] = x
+            xx["id"] = x[0:8]
             r.append(xx)
     return r
+
+def find_snapshot(snapshotID,args,basepath,masterkey):
+    ss = get_snapshots(basepath,masterkey)
+    if snapshotID != "latest":
+        for snap in ss:
+            if snapshotID == snap["id"]:
+                return snap
+        return None
+    else:
+        print ("latest not yet implemented")
+        return None
 def jsonpdumps(x):
     return json.dumps(x,sort_keys=True, indent=4, separators=(',', ': '))
 def listfiles(p):
     return [x for x in os.listdir(p) if len(x) == 64]
+
+def path2array(p):
+    return p.strip("/").split("/")                
+
+
 def main():
     objecttype = set("pack|blob|snapshot|index|key|masterkey|config|lock".split("|"))
     listobjectype = set("blobs|packs|index|snapshots|keys|locks".split("|"))
@@ -59,13 +122,22 @@ def main():
     parser.add_argument('-p')
     parser.add_argument("--json",action="store_true")
     subparsers = parser.add_subparsers(dest="command")
-    pcat = subparsers.add_parser('cat')#, aliases=['co'])
-    pcat.add_argument('type',choices=objecttype)#, aliases=['co'])
-    pcat.add_argument('ID',default=None)#, aliases=['co'])
-    plist = subparsers.add_parser('list')#, aliases=['co'])
-    plist.add_argument('type',choices=listobjectype)#, aliases=['co'])
-    psnapshots = subparsers.add_parser('snapshots')#, aliases=['co'])
-    pdemo = subparsers.add_parser('demo')#, aliases=['co'])
+    pcat = subparsers.add_parser('cat')
+    pcat.add_argument('type',choices=objecttype)
+    pcat.add_argument('ID',default=None)
+    plist = subparsers.add_parser('list')
+    plist.add_argument('type',choices=listobjectype)
+
+    pls = subparsers.add_parser('ls')
+    pls.add_argument('snapshotID')
+    pls.add_argument('path')
+
+    pdump = subparsers.add_parser('dump')
+    pdump.add_argument('snapshotID')
+    pdump.add_argument('path')
+
+    psnapshots = subparsers.add_parser('snapshots')
+    pdemo = subparsers.add_parser('demo')
 
     args = parser.parse_args()
 
@@ -101,6 +173,16 @@ def main():
             print (jsonpdumps(decrypt_config_index_snapshot(masterkey,os.path.join(args.r,"snapshots",args.ID))))
         #elif args.type == "masterkey":
         #    print jsonpdumps(dict(mac=dict(k="",p=""),encrypt="")
+        elif args.type == "blob":
+            pi = Index(args.r,masterkey)
+            if not args.ID:
+                print ("missing required ID")
+            else:
+                blob,content = pi.get(args.ID)
+                if blob["type"] == "data":
+                    print (content)
+                else:
+                    print (json.loads(content))
         else:
             print ("not implemented type",args.type)
     elif args.command == "snapshots":
@@ -111,6 +193,54 @@ def main():
             for x in ss:
                 print (dict(id=x["id"][0:8],date=x["time"],host=x["hostname"],paths=x["paths"][0]))
         pass
+    elif args.command == "ls":
+        # find snapshot using args (e.g. filter)
+        snap = find_snapshot(args.snapshotID,args,args.r,masterkey)
+        if snap is None:
+            print ("unknown snapshot")
+        else:
+            root = snap["tree"]
+            pi = Index(args.r,masterkey)
+            if args.path is not None and args.path != "/":
+                parts = path2array(args.path)             
+                node,parent = find_path(parts,root,pi)
+                if node is None:
+                    print ("missing path",args.path)
+                    return
+                if node["type"] == "file":
+                    print (args.path)
+                    return
+                else:
+                    prefix = "/".join(parts)
+                    root = node["subtree"]
+            else:
+                prefix = "/"
+            ls_recursive(root, prefix,pi)
+    elif args.command == "dump":
+        # find snapshot using args (e.g. filter)
+        snap = find_snapshot(args.snapshotID,args,args.r,masterkey)
+        if snap is None:
+            print ("unknown snapshot")
+        else:
+            root = snap["tree"]
+            pi = Index(args.r,masterkey)
+            parts = path2array(args.path)               
+            node,parent = find_path(parts,root,pi)
+            if node is None:
+                print ("missing path",args.path)
+                return
+            elif node["type"] == "file":
+                for c in node["content"]:
+                    db,dc = pi.get(c)
+                    sys.stdout.buffer.write(dc)
+            else:
+                blob, content = pi.get(node["subtree"])
+                if blob["type"] == "tree":
+                    print (json.loads(content)["nodes"])
+                else:
+                    print ("error")
+
+
     elif args.command == "list":
         # [blobs|packs|index|snapshots|keys|locks]
         if args.type == "snapshots":
